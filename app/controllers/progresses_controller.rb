e7df470ed6c7edeefeb800622dce14c22894dcfc
shigeki310 <hinahisa01240301@gmail.com>
2022-09-15 17:54:21 +0900

    解凍した時の処理

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,7 +1,26 @@
 class ProgressesController < ApplicationController
+
   def new
     @progress = Progress.new
     @question = Question.first
   end
+
+  def create
+    current_game = Game.find(params[:game_id])
+
+    progress = current_game.progresses.new(cereate_params)
+    progress.assign_sequence
+    progress.save!
+
+    redirect_to new_game_progress_path(current_game)
+  end
+
+
+  private
+
+  def cereate_params
+    params.require(:progress).permit(:question_id,:answer)
+  end
   
+
 end

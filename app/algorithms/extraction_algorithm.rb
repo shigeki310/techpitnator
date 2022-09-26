fa92a11577c3dfb72905d80f28af4e95bc21cd57
shigeki310 <hinahisa01240301@gmail.com>
2022-09-26 11:39:24 +0900

    連載終了絞り込み

--- app/algorithms/extraction_algorithm.rb
+++ app/algorithms/extraction_algorithm.rb
@@ -14,14 +14,33 @@ class ExtractionAlgorithm
       # question
       question = progress.question
 
-      # =============================================
-      # ここに絞り込み処理を実装予定 
-      # =============================================
+      case question.algorithm
+      when'serialization_end'
+        serialization_end?(progress)
+      else
+        raise Exception('Invalid algorithm. --> ' + question.algorithm.to_s)
+      end
+
 
       Rails.logger.debug('On the way query is ' + @query.to_sql.to_s)
       Rails.logger.debug('On the way comics are' + @query.pluck(:title).to_a.to_s)
     end
     @query
   end
+
+  private
+
+  def serialization_end?(progress)
+    
+    if progress.positive_answer?
+      @query = @query.where.not("comics.serialization_end_year is null")
+    end
+
+    if progress.negative_answer?
+      @query = @query.where("comics.serialization_end_year is null")
+    end
+  end
+  
+
 end
 

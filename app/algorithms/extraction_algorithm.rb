ef292e19b55a157801a8a282f70a91b3e9ef9328
shigeki310 <hinahisa01240301@gmail.com>
2022-10-01 17:42:11 +0900

    質問データ追加

--- app/algorithms/extraction_algorithm.rb
+++ app/algorithms/extraction_algorithm.rb
@@ -14,18 +14,22 @@ class ExtractionAlgorithm
       question = progress.question
 
       case question.algorithm
+      when 'genre_match'
+        genre_match(progress)
       when 'serialization_end'
         serialization_end?(progress)
+      when 'public_match'
+        public_match(progress)
       else
         raise Exception('Invalid algorithm. --> ' + question.algorithm.to_s)
-      end
-
-      Rails.logger.debug('On the way query is ' + @query.to_sql.to_s)
-      Rails.logger.debug('On the way comics are ' + @query.pluck(:title).to_a.to_s)
-
     end
-    @query
+
+    Rails.logger.debug('On the way query is ' + @query.to_sql.to_s)
+    Rails.logger.debug('On the way comics are ' + @query.pluck(:title).to_a.to_s)
+
   end
+  @query
+end
 
   private 
   
@@ -38,6 +42,31 @@ class ExtractionAlgorithm
     if progress.negative_answer?
       @query = @query.where("comics.serialization_end_year is null")
     end
+
+  end
+
+  def genre_match(progress)
+
+    if progress.positive_answer?
+      @query = @query.where("comics.genre like ?", "%#{progress.question.eval_value}%")
+    end
+
+    if progress.negative_answer?
+      @query = @query.where.not("comics.genre like ?", "%#{progress.question.eval_value}%")
+    end
+
+    def public_match(progress)
+
+      if progress.positive_answer?
+        @query = @query.where("comics.publisher like ?", "%#{progress.question.eval_value}%")
+      end
+
+      if progress.negative_answer?
+        @query = @query.where.not("comics.publisher like ?", "%#{progress.question.eval_value}%")
+      end
+
+  end
+
 
   end
 
